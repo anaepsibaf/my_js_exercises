b3411248e328a6544b6a8a2f06fdb18205dd30e7
Ana Epsiba F <anaepsiba6@gmail.com>
2022-08-17 15:39:48 +0530

    Power raised to two

--- forloop.js
+++ forloop.js
@@ -1,4 +1,7 @@
+let ToThePowerTwo= []//intializing a variable
 for(let i=0;i<=20;i++)//prints number till 20
 {
     console.log(i ** 2)//raises every number from 1 to 20 to the power of 2
+    ToThePowerTwo.push(i)
 }
+console.log(ToThePowerTwo)//prints the same data in an array
